1e82612d587a3aa3408f8949fb83ca84fb2b2a33
Martina del Pino <martudelpino@gmail.com>
2024-10-29 12:27:41 +0100

    validations and option menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int rows = 0;
+        int columns = 0;
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        while (true){
+            System.out.print("Introduce el número de filas: ");
+            rows = input.nextInt();
+            if (rows > 0 ) break;
+            else System.out.println("Debes introducir un valor mayor o igual a 0");
+        }
+
+        while (true) {
+            System.out.print("Introduce el número de columnas: ");
+            columns = input.nextInt();
+            if (columns > 0 ) break;
+            else System.out.println("Debes introducir un valor mayor o igual a 0");
+        }
+
+        int[][] gameBoard = new int[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                gameBoard[i][j] = random.nextInt(9) + 1;
+            }
+        }
+        System.out.println(Arrays.deepToString(gameBoard));
+
+        int opcion ;
+        boolean end = false;
+
+       while (true) {
+           System.out.print("seleccione una opcion: ");
+           System.out.print("0 = salir ");
+           System.out.print("1 = mostrar matriz ");
+           System.out.print("2 = poner bomba ");
+
+           opcion = input.nextInt();
+
+           switch (opcion){
+               case 0 : {
+                   System.out.println("saliendo...");
+                   end = true;
+                   return;
+               }
+               case 1 : {
+                   System.out.println("mostrar matriz");
+                   break;
+               }
+               case 2 : {
+                   System.out.println("poner bomba");
+                   break;
+               }
+               default: {
+                   System.out.println("Opción no válida. Por favor, elige 0, 1 o 2.");
+                   break;
+               }
+           }
+           input.close();
+       }
+
     }
 }
